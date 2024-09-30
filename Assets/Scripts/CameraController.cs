4c830b4afcd8b611c2cc92b5f073a444bfc55dcb
CatrionaLAnderson <120408821+CatrionaLAnderson@users.noreply.github.com>
2024-09-30 12:05:00 +0100

    here you go princess

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject player;
+    private Vector3 offset;
     void Start()
     {
-        
+        offset = transform.position;
     }
-
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        transform.position = player.transform.position + offset;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector2 moveValue;
+    public float speed;
+    void OnMove(InputValue value)
     {
-        
+        moveValue = value.Get<Vector2>();
     }
-
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
     }
 }
