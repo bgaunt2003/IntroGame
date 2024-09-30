e4aedab9a511f7c7fca28aaf05c53456bc94b72e
CatrionaLAnderson <120408821+CatrionaLAnderson@users.noreply.github.com>
2024-09-30 12:38:53 +0100

    pick up

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     public Vector2 moveValue;
     public float speed;
+    private int count;
+    void Start()
+    {
+        // Initialize the count variable to 0
+        count = 0;
+    }
+
     void OnMove(InputValue value)
     {
         moveValue = value.Get<Vector2>();
@@ -15,4 +22,14 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
         GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PickUp")
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+        }
+    }
+
 }
